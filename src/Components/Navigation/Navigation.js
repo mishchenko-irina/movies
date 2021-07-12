d1e6c4f0495206e27f90fa9a04866b916b5a657a
unknown <i.mishchenko95@gmail.com>
2021-07-12 23:02:06 +0300

    style: stylization Navigation component

--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -1,13 +1,14 @@
 import { NavLink } from 'react-router-dom';
+import s from './Navigation.module.css';
 
 const Navigation = () => (
     <div>
-      <nav>
-        <NavLink to="/"  exact>
+      <nav className={s.navigation}>
+        <NavLink to="/" className={s.link} activeClassName={s.activeLink} exact>
           Home
         </NavLink>
   
-        <NavLink to="/movies">
+        <NavLink to="/movies" className={s.link} activeClassName={s.activeLink}>
           Movies
         </NavLink>
       </nav>
